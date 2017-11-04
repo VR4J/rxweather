bae9f5e82899854b82a04a1818db4dc8d987b8f3
Tim Vonsee <tim.vonsee@me.com>
2017-11-04 22:20:43 +0100

    Yahoo Weather impl "generating" YQL

--- src/main/java/com/workshop/rxjava/weather/services/YahooWeatherService.java
+++ src/main/java/com/workshop/rxjava/weather/services/YahooWeatherService.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 public class YahooWeatherService implements WeatherService {
 
-
     private Map<String, Integer> cities;
 
     public YahooWeatherService() {
@@ -18,9 +17,19 @@ public class YahooWeatherService implements WeatherService {
         final Integer woeid = cities.getOrDefault(city,defaultVal);
 
         if(woeid == defaultVal) {
-            throw new IllegalArgumentException("Unknown city name");
+            throw new IllegalArgumentException("Unknown city: " + city);
         }
-        
+
+
+
         return null;
+    }
+
+    private String buildWeatherYQL(Integer woeid) {
+        String yql = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%" +
+                woeid +
+                "%20and%20u%3D'c'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+        return yql;
     }
 }
