5a1fa65c1a580afa891d8e71603973cc0a1ed44a
Vreijsen <joery_vreijsen@hotmail.com>
2017-11-04 22:20:03 +0100

    Add httpcomponents dependency and OpenWeatherService

--- src/main/java/com/workshop/rxjava/main.java
+++ src/main/java/com/workshop/rxjava/main.java
@@ -4,6 +4,12 @@ public class main {
     public static void main(String [] args) {
         // Get Weather Report using regular Java
 
+        // Call 3 webservices to fetch weather data
+        // * OpenWeatherMap
+        // * YahooWeather
+        // *
+
+
         // Get Weather Report using Reactive Java
     }
 }
---
+++ src/main/java/com/workshop/rxjava/weather/services/OpenWeatherMapService.java
@@ -0,0 +1,12 @@
+package com.workshop.rxjava.weather.services;
+
+public class OpenWeatherMapService implements WeatherService{
+
+    private final String token = "d690375dbb9a438376642cac51407a09";
+
+    @Override
+    public Float getWeather(String city) {
+        return null;
+    }
+
+}
