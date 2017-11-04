87e3db133cf7722d3f59bbf50f8d86095b40a9c1
Vreijsen <joery_vreijsen@hotmail.com>
2017-11-04 21:49:38 +0100

    Add starting template

---
+++ src/main/java/com/workshop/rxjava/main.java
@@ -0,0 +1,9 @@
+package com.workshop.rxjava;
+
+public class main {
+    public static void main(String [] args) {
+        // Get Weather Report using regular Java
+
+        // Get Weather Report using Reactive Java
+    }
+}
